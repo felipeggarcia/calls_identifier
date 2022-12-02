cbdb73d6cb4634d47ae8c367cd28119199760a5b
Felipe Garcia <felipegogarcia@gmail.com>
2022-12-02 17:21:01 -0300

    refac: move some files

--- lib/platform_channel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/services.dart';
-
-class PlatformChannel {
-  static const _channel = EventChannel("com.example.app/callStream");
-
-  Stream callStream() async* {
-    yield* _channel.receiveBroadcastStream();
-  }
-}
